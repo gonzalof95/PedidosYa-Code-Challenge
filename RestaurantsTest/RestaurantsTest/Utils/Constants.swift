f66747d878d76fe8bec3019779356410b7cf5f5b
gonzalof95 <gonzaloivanf@gmail.com>
2020-07-27 19:19:53 -0300

    last commit

--- RestaurantsTest/RestaurantsTest/Utils/Constants.swift
+++ RestaurantsTest/RestaurantsTest/Utils/Constants.swift
@@ -13,8 +13,8 @@ struct Constants {
     static let baseURL = "http://stg-api.pedidosya.com/public/v1"
     static let tokensPath = "/tokens"
     static let restaurantsPath = "/search/restaurants"
-    static let clientID = "trivia_f"
-    static let key = "PeY@@Tr1v1@943"
+    static let clientID = ""
+    static let key = ""
     static let mainFont = "AvenirNext-DemiBold"
     static let country = "1"
     static let mapRegion: Double = 3000
